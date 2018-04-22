ca2caf582ca389481daddab873f3d93bafd90dbb
Ary Pablo Batista <arypbatista@gmail.com>
2018-04-22 13:32:57 -0300

    Added background image

--- src/client/StoryViewer/components/Slide/Background.js
+++ src/client/StoryViewer/components/Slide/Background.js
@@ -4,7 +4,7 @@ import classes from './index.scss'
 
 const Background = () => (
   <div className={classes.Background}>
-    <img className={classes.BackgroundImage} src="http://via.placeholder.com/800x600" />
+    <img className={classes.BackgroundImage} src="https://media-cdn.tripadvisor.com/media/photo-s/0f/05/55/c8/beach-area.jpg" />
   </div>
 )
 
